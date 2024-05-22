8e5e71fef0dee6ec1765e84da63a5b05dd5beb2f
DuyThanhTonion <doduythanh22102002@gmail.com>
2024-05-22 17:36:49 +0700

    update

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
-from selenium import webdriver
-import time
-from selenium.webdriver.common.by import By
-
-# Khởi tạo trình duyệt
-driver = webdriver.Chrome()
-
-#lấy đường liên kết
-driver.get("https://xe.chotot.com/mua-ban-oto")
-time.sleep(0.5)
-
-# Tìm phần tử chứa thông tin cần trích xuất bằng class name
-element = driver.find_element(By.CLASS_NAME, 'commonStyle_adTitle__g520j ')
-
-# Trích xuất văn bản từ phần tử
-info_text = element.text
-print(info_text)
+# from selenium import webdriver
+# import time
+# from selenium.webdriver.common.by import By
+#
+# # Khởi tạo trình duyệt
+# driver = webdriver.Chrome()
+#
+# #lấy đường liên kết
+# driver.get("https://xe.chotot.com/mua-ban-oto")
+# time.sleep(0.5)
+#
+# # Tìm phần tử chứa thông tin cần trích xuất bằng class name
+# element = driver.find_element(By.CLASS_NAME, 'commonStyle_adTitle__g520j ')
+#
+# # Trích xuất văn bản từ phần tử
+# info_text = element.text
+# print(info_text)
